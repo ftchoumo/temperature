7aa6a75c446916d6a8b01590050d0d79353f62d7
Francois Tchoumo <ftchoumo@outlook.com>
2021-06-28 17:34:06 -0400

    added all posibilities

--- temperatureapp/src/main/java/com/temperature/temperatureapp/service/conversionTemperatureService.java
+++ temperatureapp/src/main/java/com/temperature/temperatureapp/service/conversionTemperatureService.java
@@ -1,7 +1,10 @@
 package com.temperature.temperatureapp.service;
+
 public class conversionTemperatureService {
 	
-public double convertFromCelsiusToFahreintTemperatue(double c) {
+// convert from Celsius	
+	
+public double convertFromCelsiusToFahrenheitTemperatue(double c) {
 		double f =0;
 		f = c *(9/5) +32;
 		return f;		
@@ -21,11 +24,72 @@ public double convertFromCelsiusToRankineTemperatue(double c) {
 	return r;
 	
 }
-	
-public double convertFromFahreintToCelsiusTemperature(double f) {
+
+//convert from Fahrenheit
+
+public double convertFromFahrenheitToCelsiusTemperature(double f) {
 	double c= 0;
 	c = (f-32)*5/9;
 	return c;
 }
 
+public double convertFromFahrenheitToKelvinTemperature(double f) {
+	double k= 0;
+	k = (f+459.67)*5/9;
+	return k;
 }
+
+public double convertFromFahrenheitToRankineTemperature(double f) {
+	double r= 0;
+	r = f + 459.67;
+	return r;
+}
+
+//convert from Kelvin
+
+public double convertFromKelvinToCelsiusTemperature(double k) {
+	double c= 0;
+	c= k - 273.15;
+	return c;
+}
+
+public double convertFromKelvinToFahrenheitTemperature(double k) {
+	double f= 0;
+	f = k *(9/5) - 459.67;
+	return f;
+}
+
+public double convertFromKelvinToRankineTemperature(double k) {
+	double r= 0;
+	r = k *(9/5);
+	return r;
+}
+
+//convert from Rankine
+
+public double convertFromRankineToCelsiusTemperature(double r) {
+	double c= 0;
+	c = r *(9/5) - 273.15;
+	return c;
+}
+
+public double convertFromRankineToFahrenheitTemperature(double r) {
+	double f= 0;
+	f = r - 459.67;
+	return f;
+}
+
+public double convertFromRankineToKelvinTemperature(double r) {
+	double k= 0;
+	k = r *(9/5);
+	return k;
+}
+
+
+}
+
+
+
+
+
+
